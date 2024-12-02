2d585ff9f8f59e5a042a5064ff9acfbe0fceb355
shark1112 <luke.lambert.1165@gmail.com>
2024-12-02 11:44:25 -0600

    making solutoin nicer

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,4 +1,3 @@
-
 l = []
 
 with open("day2.txt", "r") as file:
@@ -6,50 +5,33 @@ with open("day2.txt", "r") as file:
         temp = line.strip().split()
         l.append([int(i) for i in temp])
 
-unSafe = []
-ans = 0
-for report in l:
 
+def helper(report, un_safe):
     multi = 1 if report[1] - report[0] > 0 else -1
     curr = report[0]
 
-    temp_bool = True
     for num in report[1:]:
         is_valid = (num - curr) * multi
         curr = num
         if 0 >= is_valid or is_valid > 3:
-            temp_bool = False
-            break
-    if not temp_bool:
-        unSafe.append(report)
-    ans += temp_bool
-
-print(ans)
-
-
-def helper(newReport):
-    multi = 1 if newReport[1] - newReport[0] > 0 else -1
-    curr = newReport[0]
-
-    for num in newReport[1:]:
-        is_valid = (num - curr) * multi
-        curr = num
-        if 0 >= is_valid or is_valid > 3:
-            print(newReport)
+            un_safe.append(report)
             return False
-    print("true", newReport)
-
     return True
 
+
+ans1 = 0
+un_safe = []
+for report in l:
+    ans1 += helper(report, un_safe)
+
 ans2 = 0
-for report in unSafe:
+useless = []
+for report in un_safe:
     for i in range(len(report)):
-        temp = report[:i] + report[i+1:]
-        if helper(temp):
-            ans2+= 1
+        temp = report[:i] + report[i + 1:]
+        if helper(temp, useless):
+            ans2 += 1
             break
 
-print(ans2 + ans)
-
-
+print(ans1 + ans2)
 
