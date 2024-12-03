50018166231b443fd57ced209486a659984320ed
shark1112 <luke.lambert.1165@gmail.com>
2024-12-03 12:00:32 -0600

    day3

---
+++ Day3/day3.py
@@ -0,0 +1,33 @@
+import re
+with open("day3.txt", "r") as file:
+        s = file.read()
+
+pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+valid_instructions = re.findall(pattern, s)
+
+ans = 0
+
+for x, y in valid_instructions:
+    ans += (int(x) * int(y))
+
+print(ans)
+
+pattern2 = r"mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)"
+
+valid_instructions_2 = re.findall(pattern2, s)
+print(valid_instructions_2)
+
+add = True
+findNums = r"(\d{1,3}),(\d{1,3})"
+ans2 = 0
+for curr in valid_instructions_2:
+    if curr == "do()":
+        add = True
+    elif curr == "don't()":
+        add = False
+    elif add:
+        temp = re.findall(findNums, curr)
+        x, y = temp[0]
+        ans2 += int(x) * int(y)
+
+print(ans2)
